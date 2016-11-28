9474d2da13f0e88c9821b7bf096652970dee02ea
chenzy <chenzeyuczy@qq.com>
2016-11-28 17:07:11 +0800

    Add slash before statics files and fix some bugs in front end.

--- src/routes/router.js
+++ src/routes/router.js
@@ -9,38 +9,34 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/index', function(req, res, next) {
-  res.render('index', {content_type: 'Express'});
+  res.render('index');
 });
 
 /* Project page */
 router.get('/project', function(req, res, next) {
-	res.render('main', {content_type: 'Project'});
+	res.render('main', {content_type: 'Project', content_type_cn: '课题项目'});
 });
 
 router.get('/project/:pj_id', function(req, res, next) {
-	res.render('main', {content_type: 'Project', title: req.params['pj_id']});
+	res.render('main', {content_type: 'Project', content_type_cn: '课题项目', title: req.params['pj_id']});
 });
 
 router.get('/progress/:pj_id', function(req, res, next) {
-	res.render('main', {content_type: 'Project-Progress', title: req.params['pj_id']});
+	res.render('main', {content_type: 'Progress', content_type_cn: '项目进展', title: req.params['pj_id']});
 });
 
 /* Team page */
 router.get('/team', function(req, res, next) {
-	res.render('main', {content_type: 'Team'});
+	res.render('main', {content_type: 'Team', content_type_cn: '研究团队'});
 });
 
 router.get('/team/:tm_id', function(req, res, next) {
-	res.render('main', {content_type: 'Team', title: req.params['tm_id']});
+	res.render('main', {content_type: 'Team', content_type_cn: '研究团队', title: req.params['tm_id']});
 });
 
 /* Member page */
-router.get('/member', function(req, res, next) {
-	res.render('main', {content_type: 'Member'});
-});
-
 router.get('/member/:mb_id', function(req, res, next) {
-	res.render('main', {content_type: 'Member', title: req.params['mb_id']});
+	res.render('main', {content_type: 'Member', content_type_cn: '成员信息', title: req.params['mb_id']});
 });
 
 /* News page */
@@ -56,7 +52,7 @@ router.get('/news', function(req, res, next) {
 		}
 		console.log('Number of matched query: ', rows.length);
 		console.log(result);
-		res.render('list_view', {content_type: 'News', content_type_cn: '新闻动态', list_items: result});
+		res.render('list', {content_type: 'News', content_type_cn: '新闻动态', list_items: result});
 	});
 });
 
@@ -66,16 +62,15 @@ router.get('/news/:ne_id', function(req, res, next) {
 	connection.connect();
 	connection.query(sql, function(err, rows, fields) {
 		if (err) {
-			// throw err;
-			res.render('detail', {content_type: 'News'});
+			res.render('detail', {content_type: 'News', content_type_cn: '新闻动态'});
 			return;
 		}
 		console.log('Number of matched query: ', rows.length);
 		console.log(rows);
 		if (rows.length > 0) {
-			res.render('detail', {content_type: 'News', title: rows[0].title, detail: rows[0].content});
+			res.render('detail', {content_type: 'News', content_type_cn: '新闻动态', title: rows[0].title, detail: rows[0].content});
 		} else {
-			res.render('detail', {content_type: 'News'});
+			res.render('detail', {content_type: 'News', content_type_cn: '新闻动态'});
 		}
 	});
 	connection.end();
@@ -93,7 +88,7 @@ router.get('/notification', function(req, res, next) {
 		}
 		console.log('Number of matched query: ', rows.length);
 		console.log(result);
-		res.render('list_view', {content_type: 'Notification', content_type_cn: '最新公告', list_items: result});
+		res.render('list', {content_type: 'Notification', content_type_cn: '最新公告', list_items: result});
 	});
 	connection.end();
 });
@@ -104,16 +99,15 @@ router.get('/notification/:nt_id', function(req, res, next) {
 	connection.connect();
 	connection.query(sql, function(err, rows, fields) {
 		if (err) {
-			// throw err;
-			res.render('detail', {content_type: 'Notification'});
+			res.render('detail', {content_type: 'Notification', content_type_cn: '最新公告'});
 			return;
 		}
 		console.log('Number of matched query: ', rows.length);
 		console.log(rows);
 		if (rows.length > 0) {
-			res.render('detail', {content_type: 'Notification', title: rows[0].title, detail: rows[0].content});
+			res.render('detail', {content_type: 'Notification', content_type_cn: '最新公告', title: rows[0].title, detail: rows[0].content});
 		} else {
-			res.render('detail', {content_type: 'Notification'});
+			res.render('detail', {content_type: 'Notification', content_type_cn: '最新公告'});
 		}
 	});
 	connection.end();
@@ -121,11 +115,11 @@ router.get('/notification/:nt_id', function(req, res, next) {
 
 /* Conference page */
 router.get('/conference', function(req, res, next) {
-	res.render('main', {content_type: 'Conference'});
+	res.render('main', {content_type: 'Conference', content_type_cn: '学术交流'});
 });
 
 router.get('/conference/:cf:id', function(req, res, next) {
-	res.render('main', {content_type: 'Conference', title: req.params['cf_id']});
+	res.render('main', {content_type: 'Conference', content_type_cn: '学术交流', title: req.params['cf_id']});
 });
 
 /* Article page */
@@ -141,23 +135,23 @@ router.get('/article', function(req, res, next) {
 		}
 		console.log('Number of matched query: ', rows.length);
 		console.log(result);
-		res.render('list', {content_type: 'Article', list_items: result});
+		res.render('article', {content_type: 'Article', content_type_cn: '发表文章', list_items: result});
 	});
 	connection.end();
 });
 
 /* Data_tool page */
 router.get('/data_tool', function(req, res, next) {
-	res.render('main', {content_type: 'data_tool'});
+	res.render('main', {content_type: 'Data&Tool', content_type_cn: '数据工具'});
 });
 
 router.get('/data_tool/:dt_id', function(req, res, next) {
-	res.render('main', {content_type: 'data_tool', title: req.params['dt_id']});
+	res.render('main', {content_type: 'Data&Tool', content_type_cn: '数据工具', title: req.params['dt_id']});
 });
 
 /* User page */
 router.get('/register', function(req, res, next) {
-	res.render('register');
+	res.render('register', {content_type: 'Register', content_type_cn: '注册用户'});
 });
 
 router.post('/register', function(req, res, next) {
@@ -172,12 +166,8 @@ router.post('/logout', function(req, res, next) {
 	res.redirect('index');
 });
 
-router.get('/inform_news', function(req, res, next) {
-	res.render('inform&news');
-});
-
 router.get('/contact', function(req, res, next) {
-	res.render('contact');
+	res.render('contact', {content_type: 'Contact', content_type_cn: '联系我们'});
 });
 
 module.exports = router;
